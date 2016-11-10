c80fec78f37aaf3e5d61801cf699f7cf990b4cc6
Mauricio Quijada <majiqtresi@gmail.com>
2016-11-10 02:06:43 -0600

    Agregando funciones a thread01.c

--- kthread/thread01.c
+++ kthread/thread01.c
@@ -4,3 +4,17 @@ struct task_struct *kthread_create(int (*function)(void *data),
                                    void *data,
                                    const char name[],
                                    ...)
+
+static struct task_struct *thread1;
+
+int thread_init(void){
+    char our thread[8]="thread";
+    printk(KERN_INFO "in init");
+    thread1 = kthread_create(thread_fn,NULL,our_thread);
+    if((thread1))
+        {
+        printk(KERN_INFO "in if");
+        wake_up_process(thread1);
+        }
+    return 0;
+}
